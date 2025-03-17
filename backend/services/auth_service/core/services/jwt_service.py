10f5c3a68515d4cfc8856fc314523e90fcd7295c
Ишмаев Руслан Ильдарович <ishmaev.ruslan@sogaz.ru>
2025-03-17 15:52:50 +0300

    feat: gen jwt method

--- backend/services/auth_service/core/services/jwt_service.py
+++ backend/services/auth_service/core/services/jwt_service.py
@@ -1,10 +1,14 @@
-from authlib.jose import JsonWebKey, RSAKey
+import datetime
+
+from authlib.jose import JsonWebKey, RSAKey, jwt
 
 from services.auth_service.core.services import BaseService
 from services.auth_service.core.settings import JwtSettings
 
 
 class JWTService(BaseService):
+    exp_timedelta = datetime.timedelta(days=30)
+
     def __init__(self,jwt_settings, session=None, current_user=None):
         super(JWTService, self).__init__(session=session, current_user=current_user)
         self.__jwt_settings: JwtSettings = jwt_settings
@@ -21,4 +25,8 @@ class JWTService(BaseService):
     async def private_jwk(self) -> RSAKey:
         return self.__jwt_settings.private_key
 
-
+    async def generate_jwt(self, user_id: int, is_admin: bool = False):
+        header = {"alg": "RS256", "exp": int((datetime.datetime.now() + self.exp_timedelta).timestamp())}
+        payload = {"user_id": user_id, "is_admin": is_admin}
+        result = jwt.encode(header, payload, await self.private_jwk)
+        return result
